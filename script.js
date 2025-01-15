12ada6ec71dbd56210919cc92739ae1817122871
mitiku <mitikud@gmail.com>
2025-01-15 00:11:10 -0500

    team3 project added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("js is working");
+let question = document.getElementById("question");
+let choice1 = document.getElementById("choice1");
+let choice2 = document.getElementById("choice2");
+let choice3 = document.getElementById("choice3");
+let choice4 = document.getElementById("choice4");
+
+document.getElementById("add_btn").addEventListener("click", function () {
+  displayQuestion();
+});
+
+function displayQuestion() {
+  let val = question.value;
+  if (val) {
+    console.log(question.value);
+  }
+}
